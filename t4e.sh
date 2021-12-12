da112b86b36dac3a675d699bea7f08ec4e48e365
Nam Nguyen <namnguyen61031@gmail.com>
2021-12-12 17:17:53 +0700

    Save t4e.sh

--- t4e.sh
+++ t4e.sh
@@ -2,8 +2,11 @@
 
 # infer
 docker run --shm-size=8gb -v /home/namntse05438/AIChallenges/Zalo5K/data/:/data \
--v /home/namntse05438/AIChallenges/Zalo5K/Zalo5KChallenge/result:/result \
- t4e_zalo /bin/bash /model/predict.sh
+            -v /home/namntse05438/AIChallenges/Zalo5K/Zalo5KChallenge/result:/result \
+            t4e_zalo /bin/bash /model/predict.sh
 
 # train
-docker run --shm-size=8gb -v /home/namntse05438/AIChallenges/Zalo5K/data/:/data -v /home/namntse05438/AIChallenges/Zalo5K/Zalo5KChallenge/result:/result  -v /home/namntse05438/AIChallenges/Zalo5K/Zalo5KChallenge/trained_models:/trained_models t4e_zalo python /model/train_clf_models.py
+docker run --shm-size=8gb -v /home/namntse05438/AIChallenges/Zalo5K/data/:/data \
+            -v /home/namntse05438/AIChallenges/Zalo5K/Zalo5KChallenge/result:/result  \
+            -v /home/namntse05438/AIChallenges/Zalo5K/Zalo5KChallenge/trained_models:/trained_models 
+            t4e_zalo python /model/train_clf_models.py
